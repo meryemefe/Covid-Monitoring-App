9f2e7b3f048540dd0e103e04498f52431121bea1
Ibrahim Elmas <ibrahimelmas.3452@gmail.com>
2021-05-09 00:13:05 +0300

    cors is enabled

--- src/main/java/com/bilkent/covidmonitoringservice/controller/AuthenticationController.java
+++ src/main/java/com/bilkent/covidmonitoringservice/controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin (origins = "*")
 @RequestMapping("/api/auth")
 public class AuthenticationController {
     @Autowired
